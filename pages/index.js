d367d037b06fac2ca507695029ed7631fd1463db
Anzah <anzahkhan@gmail.com>
2021-10-24 23:05:23 +1000

    added temp login logic

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
 import TodoApp from "../Components/TodoApp";
 import supabase from "../utils/initSupabase";
 
-export default function Home({ user }) {
-  console.log(user);
+import { useEffect, useState } from "react";
+import { useRouter } from "next/dist/client/router";
+
+export default function Home() {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  // Potentially add to the API
+  const fetchProfile = async () => {
+    const profile = await supabase.auth.user();
+    if (!profile) router.push("/login");
+    else setUser(profile);
+  };
+
   return (
     <main className="container mx-auto py-5 px-5 md:px-0 flex justify-center items-center">
       <TodoApp />
-      <p>{user}</p>
+      <p>{user && user.email}</p>
     </main>
   );
-}
-export async function getServerSideProps({ req }) {
-  const { user, error } = await supabase.auth.api.getUserByCookie(req);
-  console.log(error);
-
-  console.log(user);
-
-  // if (!user) {
-  //   console.log(user);
-  //   // If no user, redirect to index.
-  //   return { props: {}, redirect: { destination: "/login", permanent: false } };
-  // }
-
-  // If there is a user, return it.
-  return { props: { user } };
 }
