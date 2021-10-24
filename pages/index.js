5100c5eb527fed82a7226f57f338e61700aad5df
Anzah <anzahkhan@gmail.com>
2021-10-24 22:30:30 +1000

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import TodoApp from '../Components/TodoApp';
+import TodoApp from "../Components/TodoApp";
+import supabase from "../utils/initSupabase";
 
-export default function Home() {
+export default function Home({ user }) {
+  console.log(user);
   return (
     <main className="container mx-auto py-5 px-5 md:px-0 flex justify-center items-center">
       <TodoApp />
+      <p>{user}</p>
     </main>
-  )
+  );
+}
+export async function getServerSideProps({ req }) {
+  const { user, error } = await supabase.auth.api.getUserByCookie(req);
+  console.log(error);
+
+  console.log(user);
+
+  // if (!user) {
+  //   console.log(user);
+  //   // If no user, redirect to index.
+  //   return { props: {}, redirect: { destination: "/login", permanent: false } };
+  // }
+
+  // If there is a user, return it.
+  return { props: { user } };
 }
--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,38 @@
 import { Button } from "@supabase/ui";
 import supabase from "../utils/initSupabase";
 
-export default function AuthBasic() {
+export default function Login() {
+  const handleGithubLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { user, session, error } = await supabase.auth.signIn({
+        provider: "github",
+      });
+
+      if (error) throw error;
+    } catch (e) {
+      alert(e.error_description || e.message);
+    }
+  };
+
   return (
     <main className="container mx-auto py-5 px-5 md:px-0 flex justify-center items-center">
       <section className="w-1/3 flex justify-center items-center">
-        <Button>Login With Github</Button>
+        <Button onClick={handleGithubLogin}>Login With Github</Button>
       </section>
     </main>
   );
 }
+// export async function getServerSideProps({ req }) {
+//   const { user } = await supabase.auth.api.getUserByCookie(req);
+
+//   if (user) {
+//     console.log(user);
+//     // If no user, redirect to index.
+//     return { props: {}, redirect: { destination: "/", permanent: false } };
+//   }
+
+//   // If there is a user, return it.
+//   return { props: { user } };
+// }
