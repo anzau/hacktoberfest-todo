aaa5dc2794faa907d13938d30e4dad51d6fecb27
Abdurahman Shalil <at059214@Abdurahmans-MacBook-Pro.local>
2021-10-24 13:42:59 +0200

    Remove Form

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,11 @@
-import { Auth, Typography, Button } from "@supabase/ui";
+import { Button } from "@supabase/ui";
 import supabase from "../utils/initSupabase";
 
 export default function AuthBasic() {
   return (
     <main className="container mx-auto py-5 px-5 md:px-0 flex justify-center items-center">
-      <section className="w-1/3">
-        <Auth.UserContextProvider supabaseClient={supabase}>
-          <Auth supabaseClient={supabase} providers={["github"]} redirectTo="/profile" />
-        </Auth.UserContextProvider>
+      <section className="w-1/3 flex justify-center items-center">
+        <Button>Login With Github</Button>
       </section>
     </main>
   );
